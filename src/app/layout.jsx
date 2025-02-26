45f4160335a6c668a952f06170ab2d569a41ed8a
Damien Bourcheix <damien@mbp-de-damien.home>
2025-02-26 13:42:45 +0100

    FontAwesome icons prevented from flashing big on load

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,30 +1,18 @@
 import "./globals.css";
 
-// console.log(cutiveMono);
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
 
 export default function RootLayout({ children }) {
-  return (
-    <html
-      lang="en"
-      // className={manrope.className}
-    >
-      <head></head>
-      <body>
-        <header>
-          <h1>
-            <strong>CSS</strong> Text-Shadow
-            <br />
-            <strong>Generator</strong>
-          </h1>
-        </header>
-        {children}
-      </body>
-    </html>
-  );
+  return <html lang="en">{children}</html>;
 }
 
 export const metadata = {
   title: "CSS Text-Shadow Generator",
   description:
-    "Easily preview and generate the perfect CSS text-shadow for your designs.",
+    "Easily generate and preview the perfect CSS text-shadow for your designs.",
 };
